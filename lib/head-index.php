8b04d6a0f348873f8786587b97f62c591c3c4f7e
lekhrajkr <mailtoahuman@gmail.com>
2015-09-23 10:28:15 +0530

    Create head-index.php

---
+++ lib/head-index.php
@@ -0,0 +1,27 @@
+
+<--Join me on <a target="blank" href="https://www.facebook.com/cyber.maiden">Facebook</a>-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This website was created by lekhraj kr">
+    <meta name="author" content="Lekraj">
+    <link rel="icon" href="../../favicon.ico">
+	
+	<title>Project | Aim</title>
+	
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/basic.css" rel="stylesheet">
+	<link href="assets/fontawesome/css/font-awesome.css" rel="stylesheet">
+	<script src="assets/js/bootstrap.min.js"></script>
+	<style>
+	body{
+		padding-top:90px;
+		background-color:#e0e0e0;
+		
+	}
+	</style>
+</head>
+<body>
