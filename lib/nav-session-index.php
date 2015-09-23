2beecffe3dfc3a57b174dc700bf164d25d7b37dd
lekhrajkr <mailtoahuman@gmail.com>
2015-09-23 10:26:28 +0530

    Create nav-session-index.php

---
+++ lib/nav-session-index.php
@@ -0,0 +1,22 @@
+  <nav class="shadow navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Project Aim</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="pull-right nav navbar-nav">
+            
+            <li><a href="register.php?request=signup&redirect=homepage&stype=basic&status=1#register">Register</a></li>
+            <li><a href="login.php?request=login&redirect=homepage&stype=basic&status=1#login">Login</a></li>
+            
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
