09987acd0ebc13b8d6d69454d91fed26853ed039
lekhrajkr <mailtoahuman@gmail.com>
2015-09-23 10:11:37 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require '_database/database.php';
+if(isset($_SESSION['email'])){
+	header("location:index.php");
+}
+else{
+require '_database/database.php';
+require 'lib/head-index.php';  
+require 'lib/nav-session-index.php';
+require 'dist/forms/login.php';
+if(isset($_POST["login_button"])){
+
+		require '_database/database.php';
+        $username=  $_POST['email'];
+        $password=  $_POST['password'];
+        $sql="SELECT email,password FROM user_details WHERE email='$username'AND password='$password'";
+        $result=mysqli_query($database,$sql) or die(mysqli_errno());
+        $trws= mysqli_num_rows($result);
+			if($result->num_rows > 0 ) { 
+			$rws=  mysqli_fetch_array($result);
+            $_SESSION['email']=$rws['email'];
+            $_SESSION['password']=$rws['password'];
+			header("location:index.php?user=$username&request=login&status=success"); 
+			} 
+			else {
+                header("location:login.php?redirect=homepage&request=login&status=failed#NoAcc");
+			}
+}
+}
+?>
